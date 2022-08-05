307a80b2b65a6dacbbb7470ac300ca349db63a26
Yuliia <umousee@gmail.com>
2022-08-05 19:38:11 +0200

    Fix of metrics visibility

--- src/app.js
+++ src/app.js
@@ -93,6 +93,7 @@ function getForecast(coordinates){
     let apiKey = "6f1ef73058d115e56afccee279259142";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast)
+
 }
 
 function displayTemperarture(response){
@@ -119,8 +120,8 @@ function displayTemperarture(response){
         `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
         );
     icon.setAttribute("alt",response.data.weather[0].description);
-    overview.style.display = "block";
-    metrics.style.visibility = "block";
+    overview.style.visibility = "visible";
+    metrics.style.visibility = "visible";
 
     changeBackground(response.data.weather[0].description);
     getForecast(response.data.coord);
